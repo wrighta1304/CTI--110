b02fdfe023cde6d3dda7abe47a9f58948c4fe038
wrighta1304 <55773237+wrighta1304@users.noreply.github.com>
2019-09-24 22:32:48 -0400

    Add files via upload

---
+++ P2T1_SalesPredictionFinal_AmyWright.py
@@ -0,0 +1,15 @@
+Python 3.1.4 (default, Jun 12 2011, 14:16:16) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Enter the projected sales: 1000
+The profit is $ 230.0
+>>> ================================ RESTART ================================
+>>> 
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> ================================ RESTART ================================
+>>> 
+Enter the projected sales: 10000
+The profit is $ 2,300.00
+>>> 
---
+++ P2T1_SalesPrediction_AmyWright.py
@@ -0,0 +1,16 @@
+# Sales Prodiction Assignment.
+# 9/7/2019
+# CTI-110 P2T1 - Sales Prediction
+# Amy Wright
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
+                                
+                    
